22b5c42b42d848f500f7784bae3a6d354f18d28a
Thanh <qthanh18tn@gmail.com>
2018-11-28 11:41:14 +0700

    Add feature using USART+DMA

--- New folder/main.c
+++ New folder/main.c
@@ -10,36 +10,44 @@ typedef enum {FAILED = 0, PASSED = !FAILED} TestStatus;
 TestStatus FlagCompare=FAILED;
 TestStatus Buffercmp(uint8_t* pBuffer1, uint8_t* pBuffer2, uint16_t BufferLength);
 void init_I2C1(void);
+void usart_init(void);
+
 uint8_t Write_24Cxx(uint8_t AddrSlave, uint8_t *txbuffData,  uint16_t write_address, uint32_t  NumberByteWrite );
 uint8_t Read_24Cxx(uint8_t AddrSlave, uint8_t *rxbuffData, uint16_t read_address, uint32_t  NumberByteRead );
 void delay_01ms(uint16_t period);
 uint32_t strlenbuff(uint8_t *str);
+uint8_t Write_Page(uint8_t AddrSlave, uint8_t *txbuffData, uint16_t write_address, uint32_t NumberByteWrite);
+// uint8_t WaitForStandbyState(uint8_t AddrSlave);
 
-uint8_t aTxBuffer2[] = "66666666666666666666666666666666";
-uint8_t aRxBuffer2[256]="";
+uint8_t aTxBuffer2[] = "thanh dep trai ganh btl cua 3 em kia. thay thanh dep trai du khong. chu thanh dep trai thay du lam";
+uint8_t aRxBuffer2[BUFFER_SIZE2];
 uint32_t lenbuff;
 int main(void)
 {
   /* Enable SysTick at 10ms interrupt */
   SysTick_Config(SystemCoreClock/100);
   init_I2C1();
-	//GPIO_ResetBits(GPIOD,GPIO_Pin_12);
+  //GPIO_ResetBits(GPIOD,GPIO_Pin_12);
   GPIO_SetBits(GPIOD,GPIO_Pin_12);
   GPIO_SetBits(GPIOD,GPIO_Pin_13);
   GPIO_SetBits(GPIOD,GPIO_Pin_14);
   GPIO_SetBits(GPIOD,GPIO_Pin_15);
-  delay_01ms(10000);
+
   lenbuff=strlenbuff(aTxBuffer2);
-  if ( Write_24Cxx(0xA0,aTxBuffer2,0x00A0,lenbuff)==0xFF)
-  {
-      while(1)
-      {
-        GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-        delay_01ms(10000);
-      }
-  }
-        delay_01ms(100);
-  if (Read_24Cxx(0xA0,aRxBuffer2,0x0000,256)==0xFF)
+  // if ( Write_24Cxx(0xA0,aTxBuffer2,0x0020,lenbuff)==0xFF)
+  // {
+  //     while(1)
+  //     {
+  //       GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+  //       delay_01ms(10000);
+  //     }
+  // }
+ Write_Page(0xA0,aTxBuffer2,0x0000,lenbuff);
+        delay_01ms(1000);
+  //standmode between read write
+//WaitForStandbyState(0xA0);
+  //
+  if (Read_24Cxx(0xA0,aRxBuffer2,0x0000,lenbuff)==0xFF)
   {
 
      while(1)
@@ -52,6 +60,7 @@ int main(void)
   FlagCompare=Buffercmp(aRxBuffer2,aTxBuffer2,lenbuff);
    if (FlagCompare==PASSED)
   {
+    usart_init();
       while(1)
       {
         GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
@@ -142,7 +151,7 @@ uint8_t Write_24Cxx(uint8_t AddrSlave, uint8_t *txbuffData, uint16_t write_addre
       if((timeout--) == 0) return 0xFF;
     }
 
-	I2C_GenerateSTART(I2C1, ENABLE);
+  I2C_GenerateSTART(I2C1, ENABLE);
 
        /* Test on I2C1 EV5, Start trnsmitted successfully and clear it */
   while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
@@ -187,40 +196,108 @@ uint8_t Write_24Cxx(uint8_t AddrSlave, uint8_t *txbuffData, uint16_t write_addre
     // function send multi data create by thanh
     while(NumberByteWrite)
     {
-		if(NumberByteWrite<2)
-    	{
-    		I2C_SendData(I2C1,  *txbuffData);
-  /*	 Test on I2C1 EV8 and clear it */
-  			timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
-  			while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
-  			{
-    	   /* If the timeout delay is exeeded, exit with error code */
-    			if ((timeout--) == 0)
-    	   			return 0xFF;
- 		 	}
+    if(NumberByteWrite<2)
+      {
+        I2C_SendData(I2C1,  *txbuffData);
+  /*   Test on I2C1 EV8 and clear it */
+        timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
+        while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
+        {
+         /* If the timeout delay is exeeded, exit with error code */
+          if ((timeout--) == 0)
+              return 0xFF;
+      }
       (void)I2C1->SR1;
       (void)I2C1->SR2;
-			/* Send I2C1 STOP Condition */
-			I2C_GenerateSTOP(I2C1, ENABLE);
-			/* If operation is OK, return 0 */
-			return 0;
-    	}
-    	else
-   		{
-			I2C_SendData(I2C1,  *txbuffData);
-			/* Test on I2C1 EV8 and clear it */
-			timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
-			while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
-			{
-			     /* If the timeout delay is exeeded, exit with error code */
-			  if ((timeout--) == 0)
-			     return 0xFF;
-			}
-			txbuffData++;
-		}
-		NumberByteWrite--;
+      /* Send I2C1 STOP Condition */
+      I2C_GenerateSTOP(I2C1, ENABLE);
+      /* If operation is OK, return 0 */
+      return 0;
+     }
+    else
+     {
+      I2C_SendData(I2C1,  *txbuffData);
+      /* Test on I2C1 EV8 and clear it */
+      timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
+      while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
+      {
+           /* If the timeout delay is exeeded, exit with error code */
+        if ((timeout--) == 0)
+           return 0xFF;
+      }
+      txbuffData++;
     }
+    NumberByteWrite--;
+    }
+      I2C_GenerateSTOP(I2C1, ENABLE);
+
+    return 0;
 }
+
+uint8_t Write_Page(uint8_t AddrSlave, uint8_t *txbuffData, uint16_t write_address, uint32_t NumberByteWrite)
+{
+  uint32_t Page = NumberByteWrite/32;
+  uint32_t SinglePage=NumberByteWrite%32;
+  while(Page>0)
+  {
+    if ( Write_24Cxx(AddrSlave,txbuffData,write_address,32)==0xFF)
+      {
+        while(1)
+        {
+          GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+          delay_01ms(10000);
+        }
+      }
+      delay_01ms(100);
+			txbuffData+=32;
+      write_address+=32;
+
+      Page--;
+  }
+  // else
+  if (Page==0)
+    {
+      if (Write_24Cxx(AddrSlave,txbuffData,write_address,SinglePage)==0xFF)
+      {
+       while(1)
+       {
+         GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+         delay_01ms(10000);
+       }
+      }
+    }
+
+
+  // while(Page)
+  //   {
+  //     if (Write_24Cxx(AddrSlave,txbuffData,write_address,32)==0xFF)
+  //     {
+  //      while(1)
+  //      {
+  //        GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+  //        delay_01ms(10000);
+  //      }
+  //     }
+
+  //     Page--;
+  //     txbuffData+=32;
+  //     write_address+=32;
+  //   }
+  // if (Page==0)
+  // {
+  //     if (Write_24Cxx(AddrSlave,txbuffData,write_address,SinglePage)==0xFF)
+  //     {
+  //      while(1)
+  //      {
+  //        GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+  //        delay_01ms(10000);
+  //      }
+  //     }
+  // }
+  // return 0;
+}
+
+
 uint8_t Read_24Cxx(uint8_t AddrSlave, uint8_t *rxbuffData,  uint16_t read_address, uint32_t  NumberByteRead )
 {
   uint32_t timeout = I2C_TIMEOUT_MAX;
@@ -228,7 +305,7 @@ uint8_t Read_24Cxx(uint8_t AddrSlave, uint8_t *rxbuffData,  uint16_t read_addres
   while(I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY))
     {
       if((timeout--) == 0)
-					return 0xFF;
+          return 0xFF;
     }
   I2C_GenerateSTART(I2C1, ENABLE);
        /* Test on I2C1 EV5, Start trnsmitted successfully and clear it */
@@ -289,42 +366,44 @@ uint8_t Read_24Cxx(uint8_t AddrSlave, uint8_t *rxbuffData,  uint16_t read_addres
         /* If the timeout delay is exeeded, exit with error code */
       if ((timeout--) == 0) return 0xFF;
   }
-		timeout = I2C_TIMEOUT_MAX;
+    timeout = I2C_TIMEOUT_MAX;
 //receive multi byte
-	while(NumberByteRead)
-	{
-		//receive one byte
-		if(NumberByteRead<2)
-		{
-			I2C_AcknowledgeConfig(I2C1, DISABLE);
-			/* Test on I2C1 EV7 and clear it */
-			timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
-			while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED))
-			{
-			      /* If the timeout delay is exeeded, exit with error code */
-			      if ((timeout--) == 0) return 0xFF;
-			}
-			I2C_GenerateSTOP(I2C1, ENABLE);
-			/* Receive the Data */
-			 *rxbuffData= I2C_ReceiveData(I2C1);
-			/*!< Re-Enable Acknowledgement to be ready for another reception */
-			I2C_AcknowledgeConfig(I2C1, ENABLE);
-			return 0;
-		}
-		else
-		{
-			timeout = I2C_TIMEOUT_MAX;
-			while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED))
-			{
-			      /* If the timeout delay is exeeded, exit with error code */
-			    if ((timeout--) == 0) return 0xFF;
-			}
-			*rxbuffData= I2C_ReceiveData(I2C1);
-			rxbuffData++;
-		}
-		NumberByteRead--;
+  while(NumberByteRead)
+  {
+    //receive one byte
+    if(NumberByteRead<2)
+    {
+      I2C_AcknowledgeConfig(I2C1, DISABLE);
+      /* Test on I2C1 EV7 and clear it */
+      timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
+      while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED))
+      {
+            /* If the timeout delay is exeeded, exit with error code */
+            if ((timeout--) == 0) return 0xFF;
+      }
+      I2C_GenerateSTOP(I2C1, ENABLE);
+      /* Receive the Data */
+       *rxbuffData= I2C_ReceiveData(I2C1);
+      /*!< Re-Enable Acknowledgement to be ready for another reception */
+      I2C_AcknowledgeConfig(I2C1, ENABLE);
 
-	}
+      return 0;
+    }
+    else
+    {
+      timeout = I2C_TIMEOUT_MAX;
+      while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED))
+      {
+            /* If the timeout delay is exeeded, exit with error code */
+          if ((timeout--) == 0) return 0xFF;
+      }
+      *rxbuffData= I2C_ReceiveData(I2C1);
+      rxbuffData++;
+    }
+    NumberByteRead--;
+
+  }
+  return 0;
 }
 
 TestStatus Buffercmp(uint8_t* pBuffer1, uint8_t* pBuffer2, uint16_t BufferLength)
@@ -362,12 +441,87 @@ void delay_01ms(uint16_t period){
 
 uint32_t strlenbuff(uint8_t *str)
 {
-	uint32_t counter=0;
-	while(*str!='\0')
-		{
-			counter++;
-			str++;
-		}
-	return counter;
+  uint32_t counter=0;
+  while(*str!='\0')
+    {
+      counter++;
+      str++;
+    }
+  return counter;
+
+}
+
+void usart_init(void)
+{
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
+  DMA_InitTypeDef   DMA_InitStructure;
+  NVIC_InitTypeDef  NVIC_InitStructure;
+
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  /* Enable UART clock */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+  /* Enable DMA1 clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
+
+  /* Connect UART4 pins to AF2 */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_UART4);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_UART4);
+
+  /* GPIO Configuration for UART4 Tx */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* GPIO Configuration for USART Rx */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* USARTx configured as follow:
+    - BaudRate = 115200 baud
+    - Word Length = 8 Bits
+    - One Stop Bit
+    - No parity
+    - Hardware flow control disabled (RTS and CTS signals)
+    - Receive and transmit enabled
+  */
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_Init(UART4, &USART_InitStructure);
+
+  /* Enable USART */
+  USART_Cmd(UART4, ENABLE);
+  /* Enable UART4 DMA */
+  /* DMA1 Stream2 Channel4 for USART4 Tx configuration */
+  USART_DMACmd(UART4, USART_DMAReq_Tx, ENABLE);
+
+
+  /* DMA1 Stream2 Channel4 for USART4 Rx configuration */
+  DMA_InitStructure.DMA_Channel = DMA_Channel_4;
+  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&UART4->DR;
+  DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)aRxBuffer2;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+  DMA_InitStructure.DMA_BufferSize = BUFFER_SIZE2;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;//DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+  DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+  DMA_Init(DMA1_Stream4, &DMA_InitStructure);
+  DMA_Cmd(DMA1_Stream4, ENABLE);
 
 }
--- main.c
+++ main.c
@@ -10,6 +10,8 @@ typedef enum {FAILED = 0, PASSED = !FAILED} TestStatus;
 TestStatus FlagCompare=FAILED;
 TestStatus Buffercmp(uint8_t* pBuffer1, uint8_t* pBuffer2, uint16_t BufferLength);
 void init_I2C1(void);
+void usart_init(void);
+
 uint8_t Write_24Cxx(uint8_t AddrSlave, uint8_t *txbuffData,  uint16_t write_address, uint32_t  NumberByteWrite );
 uint8_t Read_24Cxx(uint8_t AddrSlave, uint8_t *rxbuffData, uint16_t read_address, uint32_t  NumberByteRead );
 void delay_01ms(uint16_t period);
@@ -17,8 +19,8 @@ uint32_t strlenbuff(uint8_t *str);
 uint8_t Write_Page(uint8_t AddrSlave, uint8_t *txbuffData, uint16_t write_address, uint32_t NumberByteWrite);
 // uint8_t WaitForStandbyState(uint8_t AddrSlave);
 
-uint8_t aTxBuffer2[] = "00000000001111111111222222222233333333330000000000111111111122222222223333333333";
-uint8_t aRxBuffer2[256]="";
+uint8_t aTxBuffer2[] = "thanh dep trai ganh btl cua 3 em kia. thay thanh dep trai du khong. chu thanh dep trai thay du lam";
+uint8_t aRxBuffer2[BUFFER_SIZE2];
 uint32_t lenbuff;
 int main(void)
 {
@@ -45,7 +47,7 @@ int main(void)
   //standmode between read write
 //WaitForStandbyState(0xA0);
   //
-  if (Read_24Cxx(0xA0,aRxBuffer2,0x0000,256)==0xFF)
+  if (Read_24Cxx(0xA0,aRxBuffer2,0x0000,lenbuff)==0xFF)
   {
 
      while(1)
@@ -58,6 +60,7 @@ int main(void)
   FlagCompare=Buffercmp(aRxBuffer2,aTxBuffer2,lenbuff);
    if (FlagCompare==PASSED)
   {
+    usart_init();
       while(1)
       {
         GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
@@ -448,35 +451,77 @@ uint32_t strlenbuff(uint8_t *str)
 
 }
 
-// uint8_t WaitForStandbyState(uint8_t AddrSlave)
-// {
-// 	uint32_t timeout = I2C_TIMEOUT_MAX;
+void usart_init(void)
+{
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
+  DMA_InitTypeDef   DMA_InitStructure;
+  NVIC_InitTypeDef  NVIC_InitStructure;
 
-//  while(I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY))
-//     {
-//       if((timeout--) == 0)
-//           return 0xFF;
-//     }
-//   I2C_GenerateSTART(I2C1, ENABLE);
-//        /* Test on I2C1 EV5, Start trnsmitted successfully and clear it */
-//   while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
-//        {
-//             /* If the timeout delay is exeeded, exit with error code */
-//           if ((timeout--) == 0)
-//             return 0xFF;
-//        }
-//        //send address slave and mode transmitter to write data
-//   I2C_Send7bitAddress(I2C1, AddrSlave, I2C_Direction_Transmitter);
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  /* Enable UART clock */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+  /* Enable DMA1 clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
 
-//   timeout = I2C_TIMEOUT_MAX; /* Initialize timeout value */
-//   while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED))
-//        {
-//            /* If the timeout delay is exeeded, exit with error code */
-//           if ((timeout--) == 0)
-//           return 0xFF;
-//        }
-//        (void)I2C1->SR2;
-//       I2C_GenerateSTOP(I2C1, ENABLE);
-//       return 0;
+  /* Connect UART4 pins to AF2 */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_UART4);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_UART4);
 
-// }
+  /* GPIO Configuration for UART4 Tx */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* GPIO Configuration for USART Rx */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* USARTx configured as follow:
+    - BaudRate = 115200 baud
+    - Word Length = 8 Bits
+    - One Stop Bit
+    - No parity
+    - Hardware flow control disabled (RTS and CTS signals)
+    - Receive and transmit enabled
+  */
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART_Init(UART4, &USART_InitStructure);
+
+  /* Enable USART */
+  USART_Cmd(UART4, ENABLE);
+  /* Enable UART4 DMA */
+  /* DMA1 Stream2 Channel4 for USART4 Tx configuration */
+  USART_DMACmd(UART4, USART_DMAReq_Tx, ENABLE);
+
+
+  /* DMA1 Stream2 Channel4 for USART4 Rx configuration */
+  DMA_InitStructure.DMA_Channel = DMA_Channel_4;
+  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&UART4->DR;
+  DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)aRxBuffer2;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+  DMA_InitStructure.DMA_BufferSize = BUFFER_SIZE2;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;//DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+  DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+  DMA_Init(DMA1_Stream4, &DMA_InitStructure);
+  DMA_Cmd(DMA1_Stream4, ENABLE);
+
+}
